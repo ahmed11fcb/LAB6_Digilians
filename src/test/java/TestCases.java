01214f616ef5e8628bdab20bd0c2a0d8384aca1b
ayamohamed-22 <ayasodany2002@gmail.com>
2026-05-17 16:44:52 +0300

    aya

--- src/test/java/TestCases.java
+++ src/test/java/TestCases.java
@@ -3,14 +3,13 @@ import org.testng.annotations.Test;
 public class TestCases {
 
     @Test
-    public void MyFirstTestCase()
-    {
+    public void MyFirstTestCase() {
         System.out.println("hello from ahmed hamdy");
         System.out.println("hello from mohamed sobhy");
     }
 
 
-    }
+}
 
 
 
